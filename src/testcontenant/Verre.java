04047499224d2d7fdbe358a616e942fe99e9a742
Adrien VARET <adrien.varet@etu.univ-amu.fr>
2020-10-20 11:12:58 +0200

    suppression testcontenant

--- src/testcontenant/Verre.java
+++
@@ -1,9 +0,0 @@
-package testcontenant;
-
-public class Verre extends Contenant {
-    Verre(int contenance){
-        super(contenance, 0);
-    }
-    void boire(int quantite) {
-        setReste(getReste() - quantite);
-} }
